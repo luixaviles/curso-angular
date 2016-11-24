2fe969a7f93fd6a32831c586f8da1849f38e7beb
luixaviles <luis.avilesz@gmail.com>
2016-11-23 21:15:32 -0400

    Adding unit tests

--- client/app/main/list/angular/edit-dialog/edit-dialog.controller.spec.js
+++ client/app/main/list/angular/edit-dialog/edit-dialog.controller.spec.js
@@ -1,21 +1,68 @@
 'use strict';
 
-describe('Controller: EditDialogCtrl', function () {
+describe('Controller: EditDialogController', function () {
 
   // load the controller's module
-  beforeEach(module('projectApp'));
+  beforeEach(module('uiApp'));
 
-  var EditDialogCtrl, scope;
+  var controller,
+    $uibModalInstance,
+    $q,
+    scope,
+    Course,
+    deferred;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope,_$q_, _Course_) {
     scope = $rootScope.$new();
-    EditDialogCtrl = $controller('EditDialogCtrl', {
-      $scope: scope
+    Course = _Course_;
+    $q = _$q_;
+    deferred = $q.defer();
+
+    // spyOn(Course, 'update').and.callThrough();
+    spyOn(Course, 'update').and.returnValue(deferred.promise);
+
+    $uibModalInstance =
+      jasmine.createSpyObj('$uibModalInstance', [
+        'close',
+        'dismiss'
+      ]);
+    controller = $controller('AngularEditController', {
+      $scope: scope,
+      $uibModalInstance: $uibModalInstance,
+      data: {
+
+      },
+      Course: Course
     });
   }));
 
-  it('should ...', function () {
-    expect(1).toEqual(1);
+  it('should allow fill data', function () {
+    expect(controller.selected).toBeDefined();
+  });
+
+  it('should allow save changes', function () {
+    deferred.resolve({
+      status: 200
+    });
+    // deferred.reject({
+    //   status: 401
+    // });
+    
+    controller.saveChanges();
+    expect(Course.update).toHaveBeenCalled();
+  });
+
+  it('should allow close dialog', function () {
+    controller.close();
+    expect($uibModalInstance.close).toHaveBeenCalled();
+  });
+
+
+  it('should allow open date selector', function () {
+    expect(controller.isDateOpened).toBe(false);
+    expect(controller.isDateOpened).toBeFalsy();
+    controller.openDateSelector();
+    expect(controller.isDateOpened).toBeTruthy();
   });
 });
