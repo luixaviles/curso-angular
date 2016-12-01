e24acb50b77550cb54f7aa7a31f15d34a3f72910
luixaviles <luis.avilesz@gmail.com>
2016-11-30 21:09:20 -0400

    Adding protractor tests

--- protractor.conf.js
+++ protractor.conf.js
@@ -11,6 +11,7 @@ var config = {
   // A base URL for your application under test. Calls to protractor.get()
   // with relative paths will be prepended with this.
   baseUrl: 'http://localhost:' + (process.env.PORT || '9000'),
+  directConnect: true,
 
   // Credientials for Saucelabs
   sauceUser: process.env.SAUCE_USERNAME,
